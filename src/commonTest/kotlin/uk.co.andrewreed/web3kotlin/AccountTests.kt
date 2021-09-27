507166f36d64ee57fbde9d3dee6d18bb46e0271f
Andrew Reed <andrew_reed@hotmail.com>
2021-09-27 19:10:48 +0100

    Added test for account

--- src/commonTest/kotlin/uk.co.andrewreed/web3kotlin/AccountTests.kt
+++ src/commonTest/kotlin/uk.co.andrewreed/web3kotlin/AccountTests.kt
@@ -1,4 +1,15 @@
 package uk.co.andrewreed.web3kotlin
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class AccountTests {
+
+    @Test
+    fun testSimplePrivateKey() = runTest {
+        val account = Account.privateKeyToAccount("fa982e3142ff4bec6fa35c1c3bf90e2843be5ce43f3e2bd71d2e5957b358a3f0")
+        assertEquals("0x7DcdB62d6162A5eD4aD035b8cbCB04444ce445DC", account.address)
+        assertEquals("7DcdB62d6162A5eD4aD035b8cbCB04444ce445DC", account.publicKey)
+    }
+
 }
