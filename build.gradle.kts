f1a1e7d17653d21a9d6a24269087b61f016d4108
Andrew Reed <andrew_reed@hotmail.com>
2021-09-27 21:22:01 +0100

    Added credentials to repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,14 @@ group = "uk.co.andrewreed"
 version = project_version
 
 repositories {
+    maven {
+        name = "github"
+        url = uri("https://maven.pkg.github.com/reedyuk/jsonrpc-kotlin-client")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        }
+    }
     google()
     jcenter()
     mavenCentral()
