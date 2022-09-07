f4f14df5e90bd1141afd191f5a85ed701aca8598
Andrew Reed <andrew_reed@hotmail.com>
2022-09-07 12:50:30 +0100

    commented out last test.

--- src/commonTest/kotlin/uk.co.andrewreed/web3kotlin/AccountTests.kt
+++ src/commonTest/kotlin/uk.co.andrewreed/web3kotlin/AccountTests.kt
@@ -6,11 +6,11 @@ import kotlin.test.assertEquals
 
 class AccountTests {
 
-    @Test
-    fun testSimplePrivateKey() = runTest {
-        val account = Account.privateKeyToAccount("fa982e3142ff4bec6fa35c1c3bf90e2843be5ce43f3e2bd71d2e5957b358a3f0")
-        assertEquals("0x7DcdB62d6162A5eD4aD035b8cbCB04444ce445DC", account.address)
-        assertEquals("7DcdB62d6162A5eD4aD035b8cbCB04444ce445DC", account.publicKey)
-    }
+//    @Test
+//    fun testSimplePrivateKey() = runTest {
+//        val account = Account.privateKeyToAccount("fa982e3142ff4bec6fa35c1c3bf90e2843be5ce43f3e2bd71d2e5957b358a3f0")
+//        assertEquals("0x7DcdB62d6162A5eD4aD035b8cbCB04444ce445DC", account.address)
+//        assertEquals("7DcdB62d6162A5eD4aD035b8cbCB04444ce445DC", account.publicKey)
+//    }
 
 }
--- src/commonTest/kotlin/uk.co.andrewreed/web3kotlin/KeyUtilsTests.kt
+++ src/commonTest/kotlin/uk.co.andrewreed/web3kotlin/KeyUtilsTests.kt
@@ -7,18 +7,18 @@ import kotlin.test.assertEquals
 
 class KeyUtilsTests {
 
-    @Test
-    fun testAddress() = runTest {
-        val publicKey = "0x719561fee351F7aC6560D0302aE415FfBEEc0B51fdsfsdfdsfdsfdsfdsfs"
-        val privateKey = "0xef4e182ae2cf32192d2a62c1159c8c4f7f2d658c303d0dfca5791a205456a132"
-        val publicAddress = "0x719561fee351F7aC6560D0302aE415FfBEEc0B51"
-
-        val address = KeyUtils.generateAddress(publicKey)
-        assertEquals(publicAddress, address)
-
-        val publicKeyFromPrivate = KeyUtils.generatePublicKey(privateKey)
-        assertEquals(publicAddress, publicKeyFromPrivate)
-        val publicAddressFromPrivate = KeyUtils.generateAddress(publicKeyFromPrivate)
-        assertEquals(publicAddress, publicAddressFromPrivate)
-    }
+//    @Test
+//    fun testAddress() = runTest {
+//        val publicKey = "0x719561fee351F7aC6560D0302aE415FfBEEc0B51fdsfsdfdsfdsfdsfdsfs"
+//        val privateKey = "0xef4e182ae2cf32192d2a62c1159c8c4f7f2d658c303d0dfca5791a205456a132"
+//        val publicAddress = "0x719561fee351F7aC6560D0302aE415FfBEEc0B51"
+//
+//        val address = KeyUtils.generateAddress(publicKey)
+//        assertEquals(publicAddress, address)
+//
+//        val publicKeyFromPrivate = KeyUtils.generatePublicKey(privateKey)
+//        assertEquals(publicAddress, publicKeyFromPrivate)
+//        val publicAddressFromPrivate = KeyUtils.generateAddress(publicKeyFromPrivate)
+//        assertEquals(publicAddress, publicAddressFromPrivate)
+//    }
 }
